24ee93aa4ab02dbede8af10c621964c6ab4a1613
Michał Kaleta <mic.kaleta@gmail.com>
2018-03-15 16:56:01 +0100

    basic setup and API get user todos

--- controllers/api_controller.js
+++ controllers/api_controller.js
@@ -1,13 +1,20 @@
 const Todos = require('../models/to_do_model');
 const bodyParser= require('body-parser');
 
-
 module.exports=function(app){
 
   app.use(bodyParser.json());
-  app.get('/todos',(req,res)=>{
+  app.use(bodyParser.urlencoded({extended: true}))
 
-    res.send()
+  app.get('/api/todos/:user',(req,res)=>{
+    Todos.find({username: req.params.user},
+      (err,todos)=>{
+        
+        if (err) throw err;
+        
+        res.send(todos) 
+       
+    })
 
   })
 }
